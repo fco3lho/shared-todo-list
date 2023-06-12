d7df191dc64d0379a42ed462136ca90e9deea54c
farinellizin <lucasfcpinho@gmail.com>
2023-06-12 15:02:50 -0300

    Conexão com o Banco de Dados estabelecida em /server/index.js.

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
+// conexão com o banco de dados
+const db = require('./config/database').databaseConnection;
+
 app.listen(port, () => {
   console.log("Working...");
 });
