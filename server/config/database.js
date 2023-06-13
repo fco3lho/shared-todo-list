ce5d81e26bd3e3b80c3dcee3f936e129a7ff689b
Felipe Campos <felipecampos50123@gmail.com>
2023-06-12 22:40:23 -0300

    Instalando mysql

--- server/config/database.js
+++ server/config/database.js
@@ -1,20 +1,20 @@
-const mysql = require('mysql')
+const mysql = require("mysql");
 
 // Estabelecendo conexão
 const conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'todolist'
-})
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "todolist",
+});
 
 // Realizando conexão
 conn.connect((err) => {
-    if (err) {
-        console.log(err)
-    }
+  if (err) {
+    console.log(err);
+  }
 
-    console.log('Database connected')
-})
+  console.log("Database connected");
+});
 
 exports.databaseConnection = conn;
