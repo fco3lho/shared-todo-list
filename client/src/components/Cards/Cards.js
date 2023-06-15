3326ae2f3e29c353e54253f2a2f396a97f0b50ac
Felipe Campos <felipecampos50123@gmail.com>
2023-06-15 11:28:57 -0300

    Botão para ver tarefas de lista especifica

--- client/src/components/Cards/Cards.js
+++ client/src/components/Cards/Cards.js
@@ -5,18 +5,23 @@ import Axios from "axios";
 
 //Hooks
 import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Cards = (props) => {
-  return <>
-    <div className={styles.cardContainer}>
-      <p>ID Lista: {props.list_id}</p>
-      <p>Nome: {props.name}</p>
-      <p>Data de criação: {props.create_date}</p>
-      <p>Data da última modificação: {props.last_mod}</p>
-      <p>Usuário que fez última modificação: {props.user_last_mod_id}</p>
-      <p>Usuário administrador: {props.user_admin_id}</p>
-    </div>
-  </>;
+
+  return (
+    <>
+      <div className={styles.cardContainer}>
+        <p>ID Lista: {props.list_id}</p>
+        <p>Nome: {props.name}</p>
+        <p>Data de criação: {props.create_date}</p>
+        <p>Data da última modificação: {props.last_mod}</p>
+        <p>Usuário que fez última modificação: {props.user_last_mod_id}</p>
+        <p>Usuário administrador: {props.user_admin_id}</p>
+        <Link to={`/${props.list_id}`}>Ver tarefas</Link>
+      </div>
+    </>
+  );
 };
 
 export default Cards;
